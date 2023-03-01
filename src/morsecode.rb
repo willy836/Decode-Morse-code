b88a96789af9cfe4c14944ab91d701de95e58d35
willy836 <wilsonkimiri836@gmail.com>
2023-03-01 15:44:59 +0300

    Decode Morse char and Morse word

---
+++ src/morsecode.rb
@@ -0,0 +1,42 @@
+@morse_code = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+def decode_char(char)
+    @morse_code[char]
+end
+
+def decode_word(word)
+    @output = ''
+    word.split.each do |char|
+        @output += decode_char(char)
+    end
+    @output
+end
+
+# ADD CODE HERE
